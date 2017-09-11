0eac85b8a2eaa6d4962abc490dca7e07a2f09f47
Aaron Mendes <Aaron Mendes>
2017-09-10 18:07:50 -0700

    added all the code

---
+++ models/note.js
@@ -0,0 +1,23 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create a schema class
+var Schema = mongoose.Schema;
+
+
+var NoteSchema = new Schema({
+  
+  title: {
+    type: String
+  },
+  
+  body: {
+    type: String
+  }
+});
+
+
+
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
